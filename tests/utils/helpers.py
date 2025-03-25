cf948f0317b837730660406e2c606dd8a904592f
ARuidiaz24 <andresruidiaz24.com>
2025-03-24 19:42:00 -0500

    Se eliman helpers

--- tests/utils/helpers.py
+++
@@ -1,5 +0,0 @@
-from fixtures.browser import browser
-from playwright.sync_api import Page
-
-def screenshot (page: Page, path: str, full_page: bool = True):
-    page.screenshot(path=path, full_page=full_page)
