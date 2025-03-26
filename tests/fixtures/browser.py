20eb6ea887b3c628658e1adf2a56d41a73ded4c9
ARuidiaz24 <andresruidiaz24.com>
2025-03-26 15:31:37 -0500

    Funcionalidad de tomar video de cada test_case y guardalos en la ruta correspondiente

--- tests/fixtures/browser.py
+++ tests/fixtures/browser.py
@@ -1,9 +1,30 @@
 import pytest
 from playwright.sync_api import sync_playwright
+import os
+import shutil
 
-@pytest.fixture(scope="class")
-def browser():
+@pytest.fixture(scope="function")
+def browser(request):
+    testcase_name = request.node.name
+
+    if testcase_name.startswith("test_login"):
+        subdir = "test_login"
+    elif testcase_name.startswith("test_resetPass"):
+        subdir = "test_resetPass"
+    else:
+        subdir = "other"
+
+    video_dir = f"videos/{subdir}"
+    os.makedirs(video_dir, exist_ok=True)
+
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
-        yield browser
+        ## yield browser
+        ## browser.close()
+        context = browser.new_context(
+            record_video_dir = video_dir,
+            record_video_size = {"width": 1280, "height": 720}
+        )
+        yield context
+        context.close()
         browser.close()
